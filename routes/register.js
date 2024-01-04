ae1ee32f8fa1745d705f8290c8fbdc1f6aa617c1
Pramodya Abeysinghe <19247697+apramodya@users.noreply.github.com>
2024-01-04 11:53:54 -0700

    update route paths

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/registerController');
 
-router.post('/', registerController.handleNewUser);
+router.post('/user', registerController.handleNewUser);
+router.post('/admin', registerController.handleNewAdmin);
+router.post('/editor', registerController.handleNewEditor);
 
 module.exports = router;
